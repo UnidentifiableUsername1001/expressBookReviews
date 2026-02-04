9879324d6833005c6b9f453053cf1fda0fc8da3b
UnidentifiableUsername1001 <klquinn12@gmail.com>
2026-02-04 10:35:19 -0500

    Task 3 complete

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -27,8 +27,16 @@ public_users.get('/isbn/:isbn',function (req, res) {
   
 // Get book details based on author
 public_users.get('/author/:author',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  let matchingAuthor = [];
+
+  let keys = Object.keys(books);
+
+  keys.forEach((key) => {
+    if (books[key].author === req.params.author){
+        matchingAuthor.push(books[key]);
+    }
+  });
+  return res.status(200).send(JSON.stringify(matchingAuthor, null, 4))
 });
 
 // Get all books based on title
